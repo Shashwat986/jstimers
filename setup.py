046149f8d44d3ad2ab4961931ae898802f1479a9
Shashwat Chandra <chandra.shashwat@gmail.com>
2018-07-04 18:04:15 +0530

    Making ready for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(name='JSTimers',
       version='1.1',
       description='Javascript Timer functions for Python',
       author='Shashwat Chandra',
       author_email='chandras.2013@iitkalumni.org',
       url='https://github.com/Shashwat986/jstimers',
-      py_modules=['jstimers'],
+      packages=find_packages()
       )
